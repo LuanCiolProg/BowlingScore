910ed0615ac57d882986c88bbebff5425d8619dd
luan ciol <69855903+LuanCiolProg@users.noreply.github.com>
2020-11-03 14:58:58 -0300

    FirstCommit

---
+++ ScoreBowling.Tests/FrameTests.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Threading;
+using System.Xml;
+using Xunit;
+
+namespace ScoreBowling.Tests
+{
+    public class FrameTests
+    {
+        ScoreCalculator frame;
+
+
+        [Fact]
+        public void TodasZero()
+        {
+            frame = new ScoreCalculator();
+
+            for (int i = 0; i < 20; i++)
+            {
+                frame.Jogada(0);
+            }
+            int score = frame.Pontuação();
+            Assert.Equal(0, score);
+        }
+
+        [Fact]
+        public void TodosUm()
+        {
+            frame = new ScoreCalculator();
+
+            for (int i = 0; i < 20; i++)
+            {
+                frame.Jogada(1);
+            }
+            int score = frame.Pontuação();
+            Assert.Equal(20, score);
+        }
+        [Fact]
+        public void Spare()
+        {
+            frame = new ScoreCalculator();
+            frame.Jogada(6);
+            frame.Jogada(4);
+            frame.Jogada(2);
+            frame.Jogada(4);
+            int score = frame.Pontuação();
+            Assert.Equal(18, score);
+
+        }
+
+        [Fact]
+
+        public void Strike()
+        {
+            frame = new ScoreCalculator();
+            frame.Jogada(10);
+            frame.Jogada(6);
+            frame.Jogada(3);
+            int score = frame.Pontuação();
+            Assert.Equal(28, score);
+        }
+        
+        [Fact]
+        public void TodosStrike()
+        {
+            frame = new ScoreCalculator();
+
+            for (int i = 0; i < 12; i++)
+            {
+                frame.Jogada(10);
+            }
+            int score = frame.Pontuação();
+            Assert.Equal(300, score);
+        }
+
+    }
+}
---
+++ ScoreBowling/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ScoreBowling
+{
+    public class ScoreCalculator
+    {
+        public int[] jogadas =  new int[21];
+
+        public int jogadaAtual = 0;
+        
+        public void Jogada(int pinos)
+        {
+            jogadas[jogadaAtual] = pinos;
+            jogadaAtual++;
+        }
+        
+        public int Pontuação()
+        {
+            int indiceAtual = 0;
+            int Valortotal = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (jogadas[indiceAtual] == 10)
+                {
+                    Valortotal = Valortotal + 10 + jogadas[indiceAtual + 1] + jogadas[indiceAtual + 2];
+                    indiceAtual = indiceAtual + 1;
+                }
+                else if (jogadas[indiceAtual] + jogadas[indiceAtual + 1] == 10)
+                {
+                    Valortotal = Valortotal + 10 + jogadas[indiceAtual + 2];
+                    indiceAtual = indiceAtual + 2;
+                }
+                else
+                {
+                    Valortotal += jogadas[indiceAtual] + jogadas[indiceAtual + 1];
+                    indiceAtual = indiceAtual + 2;
+                }
+
+                
+                
+            }
+            return Valortotal;
+        }
+    }
+}
